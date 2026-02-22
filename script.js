c8f21c463db3194f777fc9aa7dcfe9de503c468f
kriska-lariska <laskinsk.kik@gmail.com>
2026-02-22 10:47:24 +0300

    добавлена веб-аналитика

---
+++ script.js
@@ -0,0 +1,16 @@
+class Picture {
+    constructor (tegs, link){
+        this.tegs = tegs
+        this.link = link
+    }
+}
+
+
+let tegs = [
+    ["portrait"], ["landshaft"], ["urban"], ["wedding"], ["nature"], ["street"], ["architecture"],
+]
+
+
+let photos = [
+    "https://i.pinimg.com/736x/d3/09/27/d3092780d3ed04663ac6e987066dea21.jpg", "https://i.pinimg.com/736x/4d/3d/86/4d3d860b2f08d168737f30de0d75ec57.jpg", "https://i.pinimg.com/736x/b9/ad/66/b9ad6653b213410d3dad27c095d576de.jpg", "https://i.pinimg.com/1200x/05/45/04/054504e4faceeb61c885dcb08e15e317.jpg", "https://i.pinimg.com/736x/35/5a/eb/355aeb9496690a5127804dc109d44f3f.jpg", "https://i.pinimg.com/736x/18/72/f2/1872f2724323516a11f42003c985cbab.jpg", "https://i.pinimg.com/736x/eb/34/0f/eb340f7b4393064770426ed95f432e1d.jpg"
+]
